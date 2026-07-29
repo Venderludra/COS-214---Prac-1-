a66ebcc344de1cda47e16a1e7fa40a45d5ded361
Venderludra <129943009+Venderludra@users.noreply.github.com>
2026-07-29 22:18:25 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -10,13 +10,27 @@
 using namespace std;
 
 int main(){
-    RestApiFactory pf;
-    Connector* f = pf.createConnector();
+    ConnectorFactory** pf = new ConnectorFactory*[3];
+    pf[0] = new PostgresFactory();
+    pf[1] = new CsvFactory();
+    pf[2] = new RestApiFactory();
 
-    for(string data : f->extract()){
-        cout << data << endl;
+    Connector* f  = nullptr;
+
+    for(int i = 0 ; i < 3 ; i++){
+        f = pf[i]->createConnector();
+        cout << "Source of Connector: " << f->getSource() << endl;
+        for(string data : f->extract()){
+            cout << data << endl;
+        }
+        delete f;
+        f = 0;
     }
 
-    delete f ;
+    for(int i = 0 ; i < 3 ;i++){
+        delete pf[i];
+    }
+
+    delete[] pf;
     return 0 ;
 }
